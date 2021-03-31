0e525b66d69a5abc97a2a6aa453fbe586e5284e3
Matthieu Mota <matthieumota@gmail.com>
2021-03-31 14:20:06 +0200

    Begin loop wordpress

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function my_theme_enqueue_styles() {
-    wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
+    // href="styles/style.css?v=< ?= time(); ? >" ==> Evite le cache du navigateur
+    wp_enqueue_style('style', get_template_directory_uri() . '/style.css', [], time());
 }
 
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
--- index.php
+++ index.php
@@ -3,4 +3,24 @@
     <h1><?php bloginfo('name'); ?></h1>
     <p><?php bloginfo('description'); ?></p>
 
+    <?php if (have_posts()) : // On vérifie s'il y a des articles
+        while (have_posts()) : the_post(); // On parcours chaque article ?>
+            <div>
+                <div>
+                    Dans <?php the_category(', '); ?>
+                    le <?php echo get_the_date('d F Y à H\hi'); ?>
+                </div>
+                <h2>
+                    <?php the_title(); ?>
+                </h2>
+                <p>
+                    <?php echo get_the_excerpt(); ?>...
+                </p>
+                <a href="<?php the_permalink(); ?>">
+                    Voir l'article
+                </a>
+            </div>
+        <?php endwhile;
+    endif; ?>
+
 <?php get_footer(); ?>
