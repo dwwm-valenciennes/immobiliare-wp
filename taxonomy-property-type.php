0fa052584d4849324bdc481ac705da43436a759b
Matthieu Mota <matthieumota@gmail.com>
2021-04-06 11:28:40 +0200

    Taxonomy page

--- taxonomy-property-type.php
+++ taxonomy-property-type.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-    <h1>Les annonces</h1>
+    <h1>Les annonces <?= single_term_title(); ?></h1>
 
     <div class="row properties">
         <?php while (have_posts()) : the_post();
